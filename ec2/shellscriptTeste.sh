52371f897d48c37d04b57765e082fc0d36b56070
Leonardo-Ferreira-costa <62809356+Leonardo-Ferreira-costa@users.noreply.github.com>
2025-02-13 16:13:17 -0300

    Novo arquivo criado no Debian

---
+++ ec2/shellscriptTeste.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo su
+curl --version 2>/dev/null
+
+if [ $? -eq 0 ]
+then
+	exit 0
+
+else
+	echo "INstalando"
+
+fi
+
+
+
+curl -X POST -d "nome=Leonardo Ferreira" https://difusaotech.com.br/lab/aws/index.php
