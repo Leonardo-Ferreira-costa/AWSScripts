2674fac59fc79b1f8770e1403902259db2a5080f
Leonardo-Ferreira-costa <62809356+Leonardo-Ferreira-costa@users.noreply.github.com>
2025-02-13 16:36:48 -0300

    Update shellscriptTeste.sh
    Colocado a instalação do curl dentro do if.

--- ec2/shellscriptTeste.sh
+++ ec2/shellscriptTeste.sh
@@ -3,13 +3,12 @@ curl --version 2>/dev/null
 
 if [ $? -eq 0 ]
 then
-	exit 0
+	echo "Instalado e pronto"
 
 else
-	echo "INstalando"
+	echo "Instalando o curl"
+ 	sudo yum install -y curl
 
 fi
-
-
 
 curl -X POST -d "nome=Leonardo Ferreira" https://difusaotech.com.br/lab/aws/index.php
